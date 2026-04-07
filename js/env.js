699bafeca828b38fc16eed9476c84e1a0d592360
endoverdosing <endoverdosing@gmail.com>
2026-04-06 23:33:34 -0700

    Updated

---
+++ js/env.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyCdZGDSIVbA9ZjSX55YLIiGoU7GNsgIjOI",
+        authDomain: "vexa-ai-chat.firebaseapp.com",
+        projectId: "vexa-ai-chat",
+        storageBucket: "vexa-ai-chat.firebasestorage.app",
+        messagingSenderId: "650867807223",
+        appId: "1:650867807223:web:55de75b58ffa41208714f2",
+        measurementId: "G-207LV4N213"
+    },
+    CLOUDINARY_CONFIG: {
+        cloudName: "djoa6ztjc",
+        uploadPreset: "vexa_uploads",
+        apiKey: "225653167387176"
+    }
+};
